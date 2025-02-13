759bf0e54e9579e7e973830d7e09695337f88fb4
leandrojofre <leandrotomasjofre@gmail.com>
2025-02-13 13:38:12 -0300

    Feat - Set up extension base

--- index.js
+++ index.js
@@ -1,63 +1,95 @@
-// The main script for the extension
-// The following are examples of some basic extension functionality
+import {extension_settings} from "../../../extensions.js";
+import {saveSettingsDebounced} from "../../../../script.js";
 
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
-import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
+// * Extension variables
 
-//You'll likely need to import some other functions from the main script
-import { saveSettingsDebounced } from "../../../../script.js";
-
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
+const extensionName = "SillyTavern-Mathcros";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
-const defaultSettings = {};
+const defaultSettings = {
+	enabled: true,
+	debug: false
+};
+const context = SillyTavern.getContext();
+
+// * Debugs methods
+
+const log = (...msg) => {
+	if (!extensionSettings.debug) return;
+	console.log("[" + extensionName + "]", ...msg);
+};
+
+// * Extension methods
 
 
- 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
-async function loadSettings() {
-  //Create the settings if they don't exist
-  extension_settings[extensionName] = extension_settings[extensionName] || {};
-  if (Object.keys(extension_settings[extensionName]).length === 0) {
-    Object.assign(extension_settings[extensionName], defaultSettings);
-  }
 
-  // Updating settings in the UI
-  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+// * Methods in charge of controlling the extension settings
+
+const settingsCallbacks = {
+	/**	Enables/Disables the extension */
+	enabled: () => {
+		// Nothing by the moment
+	}
 }
 
-// This function is called when the extension settings are changed in the UI
-function onExampleInput(event) {
-  const value = Boolean($(event.target).prop("checked"));
-  extension_settings[extensionName].example_setting = value;
-  saveSettingsDebounced();
+/** Changes a setting value and triggers a callback if there's any on settingsCallbacks. */
+function settingsBooleanButton(event) {
+	const target = event.target;
+	const value = Boolean($(target).prop("checked"));
+	const setting = target.getAttribute("mathcros-setting");
+	const callback = settingsCallbacks[setting];
+
+	extensionSettings[setting] = value;
+
+	if (callback) callback();
+
+	log("toggleSetting " + setting, value);
+	saveSettingsDebounced();
 }
 
-// This function is called when the button is clicked
-function onButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the checked setting
-  toastr.info(
-    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-    "A popup appeared because you clicked the button!"
-  );
+/**	Logs setting's values. */
+function displaySettings() {
+	console.debug("[" + extensionName + "]", `The extension is ${extensionSettings.enabled ? "active" : "not active"}`);
+	console.debug("[" + extensionName + "]", `Debug mode is ${extensionSettings.debug ? "active" : "not active"}`);
+	console.debug("[" + extensionName + "]", structuredClone(extensionSettings));
 }
 
-// This function is called when the extension is loaded
-jQuery(async () => {
-  // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+/** Append settings menu on ST and set listeners. */
+async function loadHTMLSettings() {
+	const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
 
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
-  $("#extensions_settings").append(settingsHtml);
+	$("#extensions_settings").append(settingsHtml);
 
-  // These are examples of listening for events
-  $("#my_button").on("click", onButtonClick);
-  $("#example_setting").on("input", onExampleInput);
+	// Event Listeners for the extension HTML
+	$("#mathcros-check-configuration").on("click", displaySettings);
+	$("#mathcros-activate-debug").on("input", settingsBooleanButton);
+	$("#mathcros-activate-extension").on("input", settingsBooleanButton);
 
-  // Load settings when starting things up (if you have any)
-  loadSettings();
-});
+	log("loadHTMLSettings");
+}
+
+/** Init setting values on the menu */
+function setSettings() {
+	$("#mathcros-activate-extension").prop("checked", extensionSettings.enabled).trigger("input");
+	$("#mathcros-activate-debug").prop("checked", extensionSettings.debug).trigger("input");
+
+	log("setSettings", extensionSettings);
+}
+
+// * Initialize Extension
+
+(async function initExtension() {
+
+	if (!context.extensionSettings[extensionName]) {
+	    context.extensionSettings[extensionName] = structuredClone(defaultSettings);
+	}
+
+	for (const key of Object.keys(defaultSettings)) {
+	    if (context.extensionSettings[extensionName][key] === undefined) {
+		   context.extensionSettings[extensionName][key] = defaultSettings[key];
+	    }
+	}
+
+	await loadHTMLSettings();
+	setSettings();
+})();
