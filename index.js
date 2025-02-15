48882c307594f2e2bedcceb8a3449a69d424fb12
leandrojofre <leandrotomasjofre@gmail.com>
2025-02-15 05:41:30 -0300

    Fix - Damn VScode and it whitespaces configuration

--- index.js
+++ index.js
@@ -8,161 +8,161 @@ const extensionName = "SillyTavern-Mathcros";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {
-	enabled: true,
-	debug: false
+    enabled: true,
+    debug: false
 };
 const context = SillyTavern.getContext();
 
 // * Debugs methods
 
 const log = (...msg) => {
-	if (!extensionSettings.enabled || !extensionSettings.debug) return;
-	console.log("[" + extensionName + "]", ...msg);
+    if (!extensionSettings.enabled || !extensionSettings.debug) return;
+    console.log("[" + extensionName + "]", ...msg);
 };
 
 // * Extension methods
 
 /**
      @param {String} prompt The full prompt string after it is combined
-     @returns {String} The same prompt but with the mathcros replaced
+    @returns {String} The same prompt but with the mathcros replaced
 */
 function sumVar(prompt) {
-     if (!extensionSettings.enabled) return prompt;
+    if (!extensionSettings.enabled) return prompt;
 
-     const regex = /{{sumvar::\w+( \w+)*(::-{0,1}\d+(\.\d+)?){0,1}}}/gi;
-     const matches = prompt.match(regex);
-     const results = [];
+    const regex = /{{sumvar::\w+( \w+)*(::-{0,1}\d+(\.\d+)?){0,1}}}/gi;
+    const matches = prompt.match(regex);
+    const results = [];
 
-     if (!matches || matches.length === 0) {
-         log("No match found for {{sumvar}}");
-         return prompt;
-     }
+    if (!matches || matches.length === 0) {
+        log("No match found for {{sumvar}}");
+        return prompt;
+    }
 
-     for (let i = 0; i < matches.length; i++) {
-          const values = matches[i]
-          .replace(/{{|}}|sumvar::/g, "")
-          .split(/::/g);
+    for (let i = 0; i < matches.length; i++) {
+        const values = matches[i]
+        .replace(/{{|}}|sumvar::/g, "")
+        .split(/::/g);
 
-          const vars = values[0]
-          .split(/ /g)
-          .map((val) => {
-               const variable = String(getLocalVariable(val) === '' ? getGlobalVariable(val) : getLocalVariable(val));
+        const vars = values[0]
+        .split(/ /g)
+        .map((val) => {
+            const variable = String(getLocalVariable(val) === '' ? getGlobalVariable(val) : getLocalVariable(val));
 
-               try {
+            try {
                     const parsedValue = JSON.parse(variable);
                     if (Array.isArray(parsedValue))
-                         return parsedValue
-                         .filter((value) => !isNaN(Number(value)))
-                         .reduce((acu, value) => acu + Number(value), 0);
-               } catch {
+                        return parsedValue
+                        .filter((value) => !isNaN(Number(value)))
+                        .reduce((acu, value) => acu + Number(value), 0);
+            } catch {
                     // Nothing to do...
-               }
+            }
 
-               return isNaN(Number(variable)) ? 0 : Number(variable);
-          })
-          .reduce((acu, val) => acu + val, 0);
+            return isNaN(Number(variable)) ? 0 : Number(variable);
+        })
+        .reduce((acu, val) => acu + val, 0);
 
-          const increment = Number(values[1] ?? "0");
-          const result = vars + increment;
-          results.push(result);
-          log(`sumVar -- ${values[0]} + ${increment} = `, result);
-     }
+        const increment = Number(values[1] ?? "0");
+        const result = vars + increment;
+        results.push(result);
+        log(`sumVar -- ${values[0]} + ${increment} = `, result);
+    }
 
-     for (let i = 0; i < matches.length; i++) {
-          const match = matches[i];
-          const result = results[i];
-          prompt = prompt.replace(match, result);
-     }
+    for (let i = 0; i < matches.length; i++) {
+        const match = matches[i];
+        const result = results[i];
+        prompt = prompt.replace(match, result);
+    }
 
-     return prompt
+    return prompt
 }
 
 /** List of macros to apply */
 const macros = {
-     sumvar: (prompt) => { return sumVar(prompt) }
+    sumvar: (prompt) => { return sumVar(prompt) }
 };
 
 /** This will apply all the macros to the prompt. */
 function runMacros(prompt) {
-     for (const key in macros)
-          prompt = macros[key](prompt);
+    for (const key in macros)
+        prompt = macros[key](prompt);
 
-     return prompt;
+    return prompt;
 }
 
 eventSource.on(event_types.GENERATE_AFTER_COMBINE_PROMPTS, (arg) => {
-     if (!extensionSettings.enabled) return;
-     log(event_types.GENERATE_AFTER_COMBINE_PROMPTS, arg);
-     arg.prompt = runMacros(arg.prompt);
+    if (!extensionSettings.enabled) return;
+    log(event_types.GENERATE_AFTER_COMBINE_PROMPTS, arg);
+    arg.prompt = runMacros(arg.prompt);
 })
 
 // * Methods in charge of controlling the extension settings
 
 const settingsCallbacks = {
-	/**	Enables/Disables the extension */
-	enabled: () => {
-		// Nothing by the moment
-	}
+    /**	Enables/Disables the extension */
+    enabled: () => {
+        // Nothing by the moment
+    }
 }
 
 /** Changes a setting value and triggers a callback if there's any on settingsCallbacks. */
 function settingsBooleanButton(event) {
-	const target = event.target;
-	const value = Boolean($(target).prop("checked"));
-	const setting = target.getAttribute("mathcros-setting");
-	const callback = settingsCallbacks[setting];
+    const target = event.target;
+    const value = Boolean($(target).prop("checked"));
+    const setting = target.getAttribute("mathcros-setting");
+    const callback = settingsCallbacks[setting];
 
-	extensionSettings[setting] = value;
+    extensionSettings[setting] = value;
 
-	if (callback) callback();
+    if (callback) callback();
 
-	log("toggleSetting " + setting, value);
-	saveSettingsDebounced();
+    log("toggleSetting " + setting, value);
+    saveSettingsDebounced();
 }
 
 /**	Logs setting's values. */
 function displaySettings() {
-	console.debug("[" + extensionName + "]", `The extension is ${extensionSettings.enabled ? "active" : "not active"}`);
-	console.debug("[" + extensionName + "]", `Debug mode is ${extensionSettings.debug ? "active" : "not active"}`);
-	console.debug("[" + extensionName + "]", structuredClone(extensionSettings));
+    console.debug("[" + extensionName + "]", `The extension is ${extensionSettings.enabled ? "active" : "not active"}`);
+    console.debug("[" + extensionName + "]", `Debug mode is ${extensionSettings.debug ? "active" : "not active"}`);
+    console.debug("[" + extensionName + "]", structuredClone(extensionSettings));
 }
 
 /** Append settings menu on ST and set listeners. */
 async function loadHTMLSettings() {
-	const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
+    const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
 
-	$("#extensions_settings").append(settingsHtml);
+    $("#extensions_settings").append(settingsHtml);
 
-	// Event Listeners for the extension HTML
-	$("#mathcros-check-configuration").on("click", displaySettings);
-	$("#mathcros-activate-debug").on("input", settingsBooleanButton);
-	$("#mathcros-activate-extension").on("input", settingsBooleanButton);
+    // Event Listeners for the extension HTML
+    $("#mathcros-check-configuration").on("click", displaySettings);
+    $("#mathcros-activate-debug").on("input", settingsBooleanButton);
+    $("#mathcros-activate-extension").on("input", settingsBooleanButton);
 
-	log("loadHTMLSettings");
+    log("loadHTMLSettings");
 }
 
 /** Init setting values on the menu */
 function setSettings() {
-	$("#mathcros-activate-extension").prop("checked", extensionSettings.enabled).trigger("input");
-	$("#mathcros-activate-debug").prop("checked", extensionSettings.debug).trigger("input");
+    $("#mathcros-activate-extension").prop("checked", extensionSettings.enabled).trigger("input");
+    $("#mathcros-activate-debug").prop("checked", extensionSettings.debug).trigger("input");
 
-	log("setSettings", extensionSettings);
+    log("setSettings", extensionSettings);
 }
 
 // * Initialize Extension
 
 (async function initExtension() {
 
-	if (!context.extensionSettings[extensionName]) {
-		context.extensionSettings[extensionName] = structuredClone(defaultSettings);
-	}
+    if (!context.extensionSettings[extensionName]) {
+        context.extensionSettings[extensionName] = structuredClone(defaultSettings);
+    }
 
-	for (const key of Object.keys(defaultSettings)) {
-		if (context.extensionSettings[extensionName][key] === undefined) {
-			context.extensionSettings[extensionName][key] = defaultSettings[key];
-		}
-	}
+    for (const key of Object.keys(defaultSettings)) {
+        if (context.extensionSettings[extensionName][key] === undefined) {
+            context.extensionSettings[extensionName][key] = defaultSettings[key];
+        }
+    }
 
-	await loadHTMLSettings();
-	setSettings();
+    await loadHTMLSettings();
+    setSettings();
 })();
